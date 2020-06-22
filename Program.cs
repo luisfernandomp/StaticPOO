07b3847f9e7a2eaadcfad2f10b9f108eab19b3f5
Luis Fernando <luisfernando20140@outlook.com>
2020-06-22 14:09:04 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StaticPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
